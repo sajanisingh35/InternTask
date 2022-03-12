639f1f70f31a30cac894f00cedcc2ce030d418ab
Sajani Singh <sajanisingh35@gmail.com>
2022-03-12 17:48:29 +0545

    Dynamic button component with props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
-
-import './App.css';
-import Button from './Button';
+import { useState } from "react";
+import "./App.css";
+import Button from "./Button";
 
 function App() {
+  const [button, setButton] = useState([
+    {
+      className: "btn btn-danger",
+      details: "default",
+    },
+    {
+      className: "btn btn-success btn-sm",
+      details: "Small",
+    },
+    {
+      className: "btn btn-default",
+      details: "Default",
+    },
+    {
+      className: "border-dotted active",
+      details: "Dashed",
+    },
+
+    {
+      className: "shadow-box z-depth-",
+      details: " BoxShadow",
+    },
+    {
+      className: "btn btn-link",
+      details: "Button",
+    },
+    {
+      className: "btn btn-primary-outline",
+      details: "Transparent Button",
+    },
+    {
+      className: "btn btn-danger    btn-rounded",
+      details: "Rounded Button",
+    },
+  ]);
+
   return (
     <>
-       <Button/>
+      {button.map((val) => (
+        <Button data={val} />
+      ))}
     </>
- 
-  
   );
 }
 
--- src/Button.js
+++ src/Button.js
@@ -1,56 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-
-
-
-function Button() {
+function Button(props) {
   return (
-    
     <div className="App">
-        <div className="mt-5 mb-5" justify-content= "space-between">
-        <button type="button" className="btn btn-danger">Large</button>
-        
-        <button type="button" className="btn btn-default">Default</button>
-        <button type="button" className="btn btn-success btn-sm">Small</button>
-        
-</div>
-        
-        <div className="mt-5 mb-3" justify-content= "space-between">
-        <button type="button" className="btn btn-primary">Primary</button>
-        <button type="button" className="btn btn-default">Default</button>
-        <button type="button" className="border-dotted active">Dashed</button>
-        </div>
-       
-        <button type="button" className="btn btn-link">Link</button>
-     
-       <div className="mt-5 mb-3" justify-content= "space-between">
-        <button type="button" className="shadow-box z-depth-4" justify-content="center">
-          Box-Shadow
-        </button>
-      
-        
-      
-       
-        <div className="mt-3 mb-3" >
-        <button class="btn btn-link" type="button" data-toggle="collapse"     data-target="#collapseExample" aria-expanded="false"     aria-controls="collapseExample"> Button without Border
-        </button>
-        </div>
-        <div className="mt-3 mb-3">
-<button type="button" className="btn btn-primary-outline"> Transparent Button
-</button>
-</div>
-        {/* <div className="mt-3 mb-3" >
-        <button type="button" className="btn" and style="background-color:transparent">Transparent</button>
-        </div> */}
-        <div className="mt-3 mb-3">
-        <button className="btn btn-danger    btn-rounded" >Rounded Button</button>
-        </div> 
-
-</div>
-
+      <div className="mt-5 mb-5" justify-content="space-between">
+        <button className={props.data.className}>{props.data.details}</button>
+      </div>
     </div>
-    
   );
 }
 
